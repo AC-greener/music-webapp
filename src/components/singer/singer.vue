2f6495ace968b59205da05ab91db15689deefcba
tongtong <1053673440@qq.com>
2018-10-08 17:08:03 +0800

    '更新了歌手页面'

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,86 @@
 <template>
   <div class="singer" ref="singer">
- 歌手
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
 
   export default {
-    
+    components: {
+      ListView
+    },
+    data() {
+      return {
+        singers: []
+      }
+    },
+    methods: {
+       _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(this._normalizeSinger(res.data.list))
+            this.singers = this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+
+        //向map中添加热门歌手数据
+        list.forEach((item, index) => {
+          if(index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+          if(!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+
+        // 为了得到有序列表，我们需要处理 map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        //由A到Z升序排列
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+
+        return hot.concat(ret)  //合并数组
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
   }
 
 </script>
